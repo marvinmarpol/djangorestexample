44b863a57ba0973012d1205ef1876324b28264dc
marvinmitchell <marvinmarpol@gmail.com>
2020-01-26 22:49:28 +0700

    viewsetrouters: add regular, generic, model viewsets

--- example_app/urls.py
+++ example_app/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
-from .views import article_list, article_detail, ArticleAPIView, ArticleDetailView, GenericAPIView
+from django.urls import path, include
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetailView, GenericAPIView, ArticleViewset
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('article', ArticleViewset, basename='article')
 
 urlpatterns = [
+
+    # routes using viewsets
+    path('viewset/', include(router.urls)),
+    path('viewset/<int:pk>', include(router.urls)),
+
     #path('article/', article_list),
     #path('detail/<int:pk>', article_detail),
     path('article/', ArticleAPIView.as_view()),
